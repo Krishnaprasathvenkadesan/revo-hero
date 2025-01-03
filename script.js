255cfa134979f1f4d4d0f406b99c436301fa2bb4
Krishnaprasath Venkadesan <webdev.prasath@gmail.com>
2025-01-03 23:04:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+// Optional: Close the menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
+});
